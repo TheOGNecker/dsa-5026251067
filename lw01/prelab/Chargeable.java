b411d028ba3a11bacfbf71f44aefb3409e148c73
TheOGNecker <rxts12@gmail.com>
2026-09-19 17:18:43 +0700

    Labwork 01 - Prelab

--- lw01/prelab/Chargeable.java
+++ lw01/prelab/Chargeable.java
@@ -1,3 +1,5 @@
+package lw01.prelab;
+
 public interface Chargeable {
         public abstract int calculateCharge(int units);
 }
--- lw01/prelab/Main.java
+++ lw01/prelab/Main.java
@@ -1,29 +1,31 @@
-import java.util.*;
+package lw01.prelab;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-        public static void main(String[] args){
-                System.out.println("Current working directory: " + new java.io.File(".").getAbsolutePath());
-
-                Scanner sc = new Scanner(Main.class.getResourceAsStream("lw01/prelab/jobs.txt"));
+        public static void main(String[] args) {
+                Scanner sc = new Scanner(Main.class.getResourceAsStream("/lw01/prelab/jobs.txt"));
 
                 List<PrintJob> jobs = new ArrayList<>();
 
-                while (sc.hasNext()){
+                while (sc.hasNext()) {
                         String type = sc.next();
                         String id = sc.next();
                         int pages = sc.nextInt();
 
-                        if (type.equals("COLOUR")){
+                        if (type.equals("COLOUR")) {
                                 jobs.add(new ColourPrint(id, pages));
-
-                        } else if (type.equals("MONO")){
+                        } else if (type.equals("MONO")) {
                                 jobs.add(new MonoPrint(id, pages));
                         }
                 }
-                for (PrintJob job : jobs){
+
+                for (PrintJob job : jobs) {
                         System.out.println(job.summary());
                 }
+
                 sc.close();
         }
-        
 }
